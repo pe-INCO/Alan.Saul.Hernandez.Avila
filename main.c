6becd93e7c5af00d801e29c9937dc7f768801526
Alan Saúl Hernández Avila <alan.hernandez9446@alumnos.udg.mx>
2026-04-20 09:30:49 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,36 +1,52 @@
 //
 //  main.c
-//  encuentra
+//  matriz
 //
-//  Created by Alan Saul Hernandez Avila on 25/03/26.
+//  Created by Alan Saul Hernandez Avila on 20/04/26.
 //
-
-#include <stdlib.h>
-#include <stdio.h>
 
 #include <stdio.h>
 
-int main()
+int main(void)
+
 {
-    int arreglo[10] = {5, 8, 12, 3, 7, 9, 15, 20, 1, 6};
-    int numero, i;
-    int encontrado = 0;
-
-    printf("Ingresa el numero a buscar: ");
-    scanf("%d", &numero);
-
-    // Búsqueda secuencial
-    for(i = 0; i < 10; i++) {
-        if(arreglo[i] == numero) {
-            printf("Numero encontrado en la posicion (indice): %d\n", i);
-            encontrado = 1;
-            break;
+    // Definimos constantes para las dimensiones
+    const int ESTUDIANTES = 3;
+    const int MATERIAS = 4;
+    
+    // Declaramos la matriz de números reales
+    float notas[ESTUDIANTES] [MATERIAS];
+    int i, j;
+    
+    printf("=== Registro de Notas (Matriz 3x4) ===\n");
+    
+    // Bloque de entrada de datos
+    for (i = 0; i < ESTUDIANTES; i++) {
+        printf("\nNotas para el Estudiante #%d:\n", i + 1);
+        for (j = 0; j < MATERIAS; j++) {
+            printf("  Materia %d: ", j + 1);
+            // Leemos el valor real y lo guardamos en la dirección de memoria
+            scanf("%f", &notas[i][j]);
         }
     }
-
-    if(!encontrado) {
-        printf("Numero no encontrado en el arreglo.\n");
+    
+    // Bloque de salida de datos (Mostrar la matriz)
+    printf("MATRIZ COMPLETA DE NOTAS\n");
+    
+    // Encabezados de columnas
+    printf("            ");
+    for (j = 0; j < MATERIAS; j++) {
+        printf("Mat %d     ", j + 1);
     }
-
+    
+    for (i = 0; i < ESTUDIANTES; i++) {
+        printf("Est. %d |  ", i + 1);
+        for (j = 0; j < MATERIAS; j++) {
+            // %8.2f reserva 8 espacios y muestra 2 decimales para alinear
+            printf("%8.2f  ", notas[i][j]);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
